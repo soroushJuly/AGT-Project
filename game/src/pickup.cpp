aa0487b627b9addafa484cbe788070a81df54a2a
soroushJuly <soroushjuly@gmail.com>
2023-10-13 00:38:55 +0100

    - create pickup class

---
+++ game/src/pickup.cpp
@@ -0,0 +1,16 @@
+#include "pickup.h"
+pickup::pickup(const engine::game_object_properties props) : engine::game_object(props)
+{}
+pickup::~pickup()
+{}
+void pickup::init()
+{
+	m_is_active = true;
+}
+void pickup::update(glm::vec3 c, float dt)
+{
+}
+engine::ref<pickup> pickup::create(const engine::game_object_properties& props)
+{
+	return std::make_shared<pickup>(props);
+}
---
+++ game/src/pickup.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<engine.h>
+
+class pickup : public engine::game_object {
+public:
+	pickup(const engine::game_object_properties props);
+	~pickup();
+	void init();
+	void update(glm::vec3 c, float dt);
+	bool active() const { return m_is_active; }
+
+	static engine::ref<pickup> create(const engine::game_object_properties& props);
+
+private:
+	bool m_is_active; // Indicates if pickup is active
+};
