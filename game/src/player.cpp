60e50160a619a7d4dace8dee649db661a0f87fde
soroushJuly <soroushjuly@gmail.com>
2023-10-26 21:44:02 +0100

    add third person camera

--- game/src/player.cpp
+++ game/src/player.cpp
@@ -28,7 +28,7 @@ void player::on_update(const engine::timestep& time_step)
 	m_object->set_rotation_amount(atan2(m_object->forward().x, m_object->forward().z));
 	if (engine::input::key_pressed(engine::key_codes::KEY_A)) // left
 		turn(1.0f * time_step);
-	if (engine::input::key_pressed(engine::key_codes::KEY_D)) // left
+	if (engine::input::key_pressed(engine::key_codes::KEY_D)) // right
 		turn(-1.0f * time_step);
 
 }
@@ -37,4 +37,21 @@ void player::turn(float angle)
 {
 	m_object->set_forward(glm::rotate(m_object->forward(), angle, glm::vec3(0.f, 1.f,
 		0.f)));
+}
+
+void player::update_camera(engine::perspective_camera& camera)
+{
+	const float looking_at = 6;
+	const float camera_distance_height = 2;
+	const float camera_distance_back = 3;
+	// the camera position
+	float height = m_object->position().y + camera_distance_height;
+	float camera_position_x = m_object->position().x - camera_distance_back * m_object->forward().x;
+	float camera_position_z = m_object->position().z - camera_distance_back * m_object->forward().z;
+	// the camera viewpoint
+	float camera_look_x = m_object->position().x + looking_at * m_object->forward().x ;
+	float camera_look_z = m_object->position().z + looking_at * m_object->forward().z;
+	//camera.position(glm::vec3(camera_x, height, camera_z));
+	camera.set_view_matrix(glm::vec3(camera_position_x, height, camera_position_z), glm::vec3(camera_look_x, 0, camera_look_z));
+
 }
