07a8d8e96ee855bed319721fbd49bfcfd3215625
soroushJuly <soroushjuly@gmail.com>
2023-11-06 02:29:51 +0000

    fix: lighting for the coin primitive

--- engine/src/engine/entities/shapes/coin.cpp
+++ engine/src/engine/entities/shapes/coin.cpp
@@ -31,15 +31,93 @@ engine::coin::coin() : m_size(0.02f), m_radius(0.1f)
 
 		coin_vertices.push_back(vertex);
 	}
+	std::vector<glm::vec3> normals;
 
-	// Inner Octagon
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	float theta = engine::PI * 180 * (i / 8.f);
-	//	engine::mesh::vertex vertex(glm::vec3(cos(theta), sin(theta), 0.6f), glm::vec3(0.f, 0.f, -1.f), ());
+	// We need 8 surfaces
+	normals.push_back(glm::cross(coin_vertices[0].position - coin_vertices[8].position, coin_vertices[9].position - coin_vertices[8].position));
+	normals.push_back(glm::cross(coin_vertices[1].position - coin_vertices[9].position, coin_vertices[10].position - coin_vertices[9].position));
+	normals.push_back(glm::cross(coin_vertices[2].position - coin_vertices[10].position, coin_vertices[11].position - coin_vertices[10].position));
+	normals.push_back(glm::cross(coin_vertices[3].position - coin_vertices[11].position, coin_vertices[12].position - coin_vertices[11].position));
+	normals.push_back(glm::cross(coin_vertices[4].position - coin_vertices[12].position, coin_vertices[13].position - coin_vertices[12].position));
+	normals.push_back(glm::cross(coin_vertices[5].position - coin_vertices[13].position, coin_vertices[14].position - coin_vertices[13].position));
+	normals.push_back(glm::cross(coin_vertices[6].position - coin_vertices[14].position, coin_vertices[15].position - coin_vertices[14].position));
+	normals.push_back(glm::cross(coin_vertices[7].position - coin_vertices[15].position, coin_vertices[8].position - coin_vertices[15].position));
 
-	//	coin_vertices.push_back(vertex);
-	//}
+
+
+
+
+	std::vector<mesh::vertex> side_vertices
+	{
+		// defining the vertices in every surface
+		// postitions		normal		tex coord
+		{coin_vertices[0].position,	normals[0], { 0.f, 0.f }},
+		{coin_vertices[8].position,	normals[0], { 1.f, 0.f }},
+		{coin_vertices[9].position,	normals[0], { 0.5f, 1.f }},
+
+		{coin_vertices[0].position,	normals[0], { 0.f, 0.f }},
+		{coin_vertices[9].position,	normals[0], { 1.f, 0.f }},
+		{coin_vertices[1].position,	normals[0], { 0.5f, 1.f }},
+
+		{coin_vertices[1].position,	normals[1], { 0.f, 0.f }},
+		{coin_vertices[9].position,	normals[1], { 1.f, 0.f }},
+		{coin_vertices[10].position,normals[1], { 0.5f, 1.f }},
+
+		{coin_vertices[1].position,	normals[1], { 0.f, 0.f }},
+		{coin_vertices[10].position,normals[1], { 1.f, 0.f }},
+		{coin_vertices[2].position,	normals[1], { 0.5f, 1.f }},
+
+		{coin_vertices[2].position,	normals[2], { 0.f, 0.f }},
+		{coin_vertices[10].position,normals[2], { 1.f, 0.f }},
+		{coin_vertices[11].position,normals[2], { 0.5f, 1.f }},
+
+		{coin_vertices[2].position,	normals[2], { 0.f, 0.f }},
+		{coin_vertices[11].position,normals[2], { 1.f, 0.f }},
+		{coin_vertices[3].position,	normals[2], { 0.5f, 1.f }},
+
+
+		{coin_vertices[3].position,	normals[3], { 0.f, 0.f }},
+		{coin_vertices[11].position,normals[3], { 1.f, 0.f }},
+		{coin_vertices[12].position,normals[3], { 0.5f, 1.f }},
+
+		{coin_vertices[3].position,	normals[3], { 0.f, 0.f }},
+		{coin_vertices[12].position,normals[3], { 1.f, 0.f }},
+		{coin_vertices[4].position,	normals[3], { 0.5f, 1.f }},
+
+		{coin_vertices[4].position,	normals[4], { 0.f, 0.f }},
+		{coin_vertices[12].position,normals[4], { 1.f, 0.f }},
+		{coin_vertices[13].position,normals[4], { 0.5f, 1.f }},
+
+		{coin_vertices[4].position,	normals[4], { 0.f, 0.f }},
+		{coin_vertices[13].position,normals[4], { 1.f, 0.f }},
+		{coin_vertices[5].position,	normals[4], { 0.5f, 1.f }},
+
+		{coin_vertices[5].position,	normals[5], { 0.f, 0.f }},
+		{coin_vertices[13].position,normals[5], { 1.f, 0.f }},
+		{coin_vertices[14].position,normals[5], { 0.5f, 1.f }},
+
+		{coin_vertices[5].position,	normals[5], { 0.f, 0.f }},
+		{coin_vertices[14].position,normals[5], { 1.f, 0.f }},
+		{coin_vertices[6].position,	normals[5], { 0.5f, 1.f }},
+
+		{coin_vertices[6].position,	normals[6], { 0.f, 0.f }},
+		{coin_vertices[14].position,normals[6], { 1.f, 0.f }},
+		{coin_vertices[15].position,normals[6], { 0.5f, 1.f }},
+
+		{coin_vertices[6].position,	normals[6], { 0.f, 0.f }},
+		{coin_vertices[15].position,normals[6], { 1.f, 0.f }},
+		{coin_vertices[7].position,	normals[6], { 0.5f, 1.f }},
+
+		{coin_vertices[7].position,	normals[7], { 0.f, 0.f }},
+		{coin_vertices[15].position,normals[7], { 1.f, 0.f }},
+		{coin_vertices[8].position,	normals[7], { 0.5f, 1.f }},
+
+		{coin_vertices[7].position,	normals[7], { 0.f, 0.f }},
+		{coin_vertices[8].position,	normals[7], { 1.f, 0.f }},
+		{coin_vertices[0].position,	normals[7], { 0.5f, 1.f }},
+	};
+
+	coin_vertices.insert(coin_vertices.end(), side_vertices.begin(), side_vertices.end());
 
 	std::vector<uint32_t> coin_indices =
 	{
@@ -55,22 +133,40 @@ engine::coin::coin() : m_size(0.02f), m_radius(0.1f)
 		13,12,8,
 		14,13,8,
 		15,14,8,
-		0,8,9,
-		0,9,1,
-		1,9,10,
-		1,10,2,
-		2,10,11,
-		2,11,3,
-		3,11,12,
-		3,12,4,
-		4,12,13,
-		4,13,5,
-		5,13,14,
-		5,14,6,
-		6,14,15,
-		6,15,7,
-		7,15,8,
-		7,8,0
+
+		16,17,18,
+		19,20,21,
+		22,23,24,
+		25,26,27,
+		28,29,30,
+		31,32,33,
+		34,35,36,
+		37,38,39,
+		40,41,42,
+		43,44,45,
+		46,47,48,
+		49,50,51,
+		52,53,54,
+		55,56,57,
+		58,59,60,
+		61,62,63,
+
+		//0,8,9,
+		//0,9,1,
+		//1,9,10,
+		//1,10,2,
+		//2,10,11,
+		//2,11,3,
+		//3,11,12,
+		//3,12,4,
+		//4,12,13,
+		//4,13,5,
+		//5,13,14,
+		//5,14,6,
+		//6,14,15,
+		//6,15,7,
+		//7,15,8,
+		//7,8,0
 	};
 
 	m_coin_mesh = engine::mesh::create(coin_vertices, coin_indices);
